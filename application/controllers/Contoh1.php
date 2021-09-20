5c802608417f857d7d3e8a74b11ddfd8122a6c6c
tomiprasetyo <tomi.prasetyo95@gmail.com>
2021-09-20 22:16:33 +0700

    Mengerjakan contoh1, contoh2, contoh3, menghilangkan index.php dan menambahkan file .htaccess

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Tomi Prasetyo
+        saya tinggal di Cilincing Jakarta Utara
+        Olahraga yang saya sukai adalah Jogging dan Bersepeda";
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Teman.. Mari kita belajar Web Programming II <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini adalah hasil dari pemodelan dengan metode(function) penjumlahan yaitu
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
